ea2f3e88857c849aeb79423a2f303a40e3863bb5
jason <jduong89@gmail.com>
2022-03-27 22:14:13 -0700

    adds dog create action/route

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+  def create
+    dog = Dog.new(
+      name: params[:name],
+      age: params[:age],
+      breed: params[:breed],
+    )
+    if dog.save
+      render json: { message: "Dog successfully added" }, status: :created
+    else
+      render json: { errors: dog.errors.full_messages }, status: :bad_request
+    end
+  end
 end
